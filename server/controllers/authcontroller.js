89d06e16956baefcd7104a4142bd58dc5bf387ab
Priyanshu0017 <priyanshubodana9@gmail.com>
2025-09-02 18:38:04 +0530

    aurh

--- server/controllers/authcontroller.js
+++ server/controllers/authcontroller.js
@@ -170,14 +170,16 @@ const verifyOtp = async (req, res) => {
   const { _id } = user._doc;
 
   res.status(200).json({
-    message: "OTP verified successfully",
-    user: {
-      id: user._id,
-      name: user.name,
-      role: user.role,
-      email: user.email,
-    },
-  });
+  message: 'OTP verified successfully',
+  user: {
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    phone: user.phone,
+    role: user.role,
+    token: generateToken(user._id),
+  }
+});
 };
 
 module.exports = {
